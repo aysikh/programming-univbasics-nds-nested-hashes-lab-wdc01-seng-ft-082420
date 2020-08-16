1a6f1cd718c509cbe285a22d4bbec881333896fe
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-16 13:05:51 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,11 +6,12 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-    #{DON_G = {:name => "Don Gately", :occupation => "Live-in Staff"}},
-    #{JOELLE_VD = {:name => "Joelle van Dyne", :occupation => "Radio Personality"}},
-    #{PAT_M = {:name => "Pat Monteseian", :occupation => "Staff"}},
-    #{KATE_G = {:name => "Kate Gompert", :occupation => "None"}},
-    #{BRUCE_G = {:name => "Bruce Green", :occupation => "Fan of Mildred"}}
+   # [
+    #[DON_G = {:name => "Don Gately", :occupation => "Live-in Staff"}],
+    #[JOELLE_VD = {:name => "Joelle van Dyne", :occupation => "Radio Personality"}],
+    #[PAT_M = {:name => "Pat Monteseian", :occupation => "Staff"}],
+    #[KATE_G = {:name => "Kate Gompert", :occupation => "None"}],
+    #[BRUCE_G = {:name => "Bruce Green", :occupation => "Fan of Mildred"}]
     #  nested_array = [[DON_G],[JOELLE_VD],[PAT_M],[KATE_G],[BRUCE_G]]
 
 def assembled_aoh 
@@ -24,13 +25,7 @@ end
 def literal_aoh 
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-    [
-    [DON_G = {:name => "Don Gately", :occupation => "Live-in Staff"}],
-    [JOELLE_VD = {:name => "Joelle van Dyne", :occupation => "Radio Personality"}],
-    [PAT_M = {:name => "Pat Monteseian", :occupation => "Staff"}],
-    [KATE_G = {:name => "Kate Gompert", :occupation => "None"}],
-    [BRUCE_G = {:name => "Bruce Green", :occupation => "Fan of Mildred"}]
-    ]
+
 end
 
 def aoh_lookup(aoh, row, key)
